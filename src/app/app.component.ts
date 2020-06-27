18b6549bfd0dc67067630dad3f2c0a58d375d902
chirushunt <soliddreams@gmail.com>
2020-06-27 22:04:36 +0530

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'relaxApp';
-}
+  totalTime = 7500;
+  breatheTime = (this.totalTime / 5) * 2;
+  holdTime = this.totalTime / 5;
+  breatheIn = false;
+  breatheOut = false;
+  hold = false;
+
+  breathAnimation = () => {
+    this.breatheIn = true;
+    this.breatheOut = false;
+    this.hold = false;
+
+
+    setTimeout(() => {
+        this.breatheIn = false;
+        this.breatheOut = false;
+        this.hold = true;
+
+        setTimeout(() => {
+          this.breatheIn = false;
+          this.breatheOut = true;
+          this.hold = false;
+          // container.className = 'container shrink';
+        }, this.holdTime);
+      }, this.breatheTime);
+
+      setInterval(this.breathAnimation, this.totalTime);
+  }
+
+
+    ngOnInit(): void {
+      this.breathAnimation();
+    }
+  }
+
+
